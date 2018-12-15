85807b8e0094b7f6f89e1f7d15feb8bb7d4e483c
Alexandre Ménielle <alexandre.menielle@gmail.com>
2018-12-15 12:08:14 +0100

    init project

---
+++ iOScape/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOScape
+//
+//  Created by Alexandre Ménielle on 15/12/2018.
+//  Copyright © 2018 Alexandre Ménielle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
